e1e3c40b01cba9ee5bdd0e846ba0e2fd2b6104e8
Mohammad AlMahllawi <59862214+m-imperfect@users.noreply.github.com>
2023-01-24 20:00:14 +0200

    add the repository to the manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ game 'gta5'
 
 author 'm_imperfect <owner@m-imperfect.net>'
 description 'Synchronize values between the server and the clients.'
+repository 'https://github.com/imperfect-fivem/sc-sync'
 version '1.0.0'
 
 server_script 'server.lua'
